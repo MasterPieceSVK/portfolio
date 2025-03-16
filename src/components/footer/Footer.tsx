f90f0e62891080da083bad31f6ba6ea1eef30f8b
MasterPieceSVK <maxi.jaroscak@gmail.com>
2025-03-16 19:50:19 +0100

    feat: change header background

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="font-hanalei text-[40px] text-center w-full  bg-transparent drop-shadow-lg backdrop-blur-sm text-primary mt-6 z-20">
+    <footer className="font-hanalei text-[40px] text-center w-full  bg-white/30 drop-shadow-lg backdrop-blur-sm text-primary mt-6 z-20">
       M. JAROSCAK @ {new Date().getFullYear()}
     </footer>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import HeaderMenu from "./HeaderMenu";
 export default function Header() {
   return (
     <header
-      className={`font-hanalei w-[100%] h-[83px] bg-transparent drop-shadow-lg backdrop-blur-sm sticky top-0 z-50`}
+      className={`font-hanalei w-[100%] h-[83px] bg-white/30  backdrop-blur-sm sticky top-0 z-50`}
     >
       <div className="flex justify-between items-center px-10 py-5  text-primary">
         <NameLink href="#home" text="M. Jaroscak" />
