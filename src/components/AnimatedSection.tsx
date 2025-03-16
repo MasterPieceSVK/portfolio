37a8b3bfcb8c89798d7bd66db723a756694afd0a
MasterPieceSVK <maxi.jaroscak@gmail.com>
2025-03-16 19:40:16 +0100

    fix: remove framer motion, resort back to tailwind motion library

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
 export default function AnimatedSection({
   children,
   id,
   className,
-  delay,
 }: {
   children: React.ReactNode;
   id: string;
@@ -13,27 +11,14 @@ export default function AnimatedSection({
   delay: number;
 }) {
   return (
-    <motion.section
+    <section
       id={id}
-      className={cn(className)}
-      initial={{
-        y: -200,
-        // filter: "blur(5px)",
-      }}
-      animate={{
-        y: 0,
-        // filter: "blur(0px)",
-      }}
-      viewport={{ once: true }}
-      transition={{
-        duration: 0.3,
-        filter: { delay: delay / 6 },
-        type: "spring",
-        bounce: 10,
-        damping: 6,
-      }}
+      className={cn(
+        "motion-scale-in-[1.1] motion-duration-[0.34s]/scale motion-ease-bounce",
+        className
+      )}
     >
       {children}
-    </motion.section>
+    </section>
   );
 }
