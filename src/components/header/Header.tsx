728febec8b30eb7f290b3b9ca3434701da4a94bf
MasterPieceSVK <maxi.jaroscak@gmail.com>
2025-04-05 09:08:05 +0200

    fix: change header padding

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header
       className={`font-hanalei w-[100%] h-[83px] bg-white/30  backdrop-blur-sm sticky top-0 z-40`}
     >
-      <div className="flex justify-between items-center px-10 py-5  text-primary">
+      <div className="flex justify-between items-center pl-4 pr-1 sm:px-5 lg:px-10 py-5  text-primary">
         <NameLink href="#home" text="M. Jaroscak" />
         <HeaderMenu />
       </div>
