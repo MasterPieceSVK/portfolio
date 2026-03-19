fc36d14955c72b6482c5809f2eebae989428eb42
Maximilian Jaroscak <112313437+MasterPieceSVK@users.noreply.github.com>
2026-03-19 23:31:53 +0100

    change google analytics code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,27 +43,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleAnalytics gaId="G-HXXEG0YDB9" />
+      <GoogleAnalytics gaId="G-LDT9HL9KYF" />
       <GoogleTagManager gtmId="GTM-NQ85V8DW" />
-
-      <Script
-        id="matomo"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://www.mt.rowbox.com/matomo/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '6']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();`,
-        }}
-      />
 
       <body className={`${jost.className} antialiased `}>
         {/* <noscript>
